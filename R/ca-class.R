0be0a85fcf647bba737cb178f6e02ab886d60f5f
ugroempi <groemping@beuth-hochschule.de>
2025-05-01 10:29:38 +0200

    introduced S3 class ca for covering array matrices; so far, the class has a print method only and serves for suppressing printing of excessive attributes (useful for small CAs only; a good solution for large CAs needs to be developed)

---
+++ R/ca-class.R
@@ -0,0 +1,39 @@
+#' Class ca and its methods
+#'
+#' This documentation file documents the class ca (for CAs = Covering Arrays) and its method.
+#'
+#' @rdname ca-class
+#'
+#' @aliases print.ca
+#'
+#' @method print ca
+#'
+#' @param x the object to be printed
+#' @param \dots further arguments to \code{print.default}
+#'
+#' @section Details:
+#' The print method for class \code{ca} suppresses printing of most attributes,
+#' but indicates which attributes are available.
+#'
+#' The S3 class \code{ca} is for a matrix with attributes.
+#' Common attributes are \code{origin}, \code{Call}, \code{flexible} or \code{PCAstatus}.
+#' The matrix elements are consecutive non-negative integers starting with 0 or 1,
+#' and possibly missing values that stand for flexible values, i.e.,
+#' values that can be arbitrarily chosen without deteriorating the strength of the CA.
+#'
+#' @export
+print.ca <- function(x, ...){
+   xnam <- deparse(substitute(x))
+    if (!"ca" %in% class(x))
+      stop("this print method is for class ca only")
+    attrs <- setdiff(names(attributes(x)), c("origin", "class",
+                                             "dim", "dimnames"))
+    for (a in attrs) attr(x, a) <- NULL
+    print.default(x, ...)
+    if (length(attrs) > 0) {
+      message("further attribute(s)", "(accessible with attr(",
+          xnam, ", attrname)):")
+      print(attrs)
+    }
+ }
+
