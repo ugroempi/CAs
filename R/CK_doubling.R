c6b926fca4689cdb0a16dab0b60e38f504e7c11d
ugroempi <groemping@beuth-hochschule.de>
2025-05-14 20:19:18 +0200

    made CK_doubling have a default D2 for v=3, and added an example of that sort

--- R/CK_doubling.R
+++ R/CK_doubling.R
@@ -11,11 +11,16 @@
 #'
 #' @usage CK_doubling(D3, D2=NULL, check=FALSE, start0=TRUE, ...)
 #'
-#' @param D3 a strength 3 CA with k columns in v levels, coded as integers 0, ..., v-1 or 1,...,v
-#' @param D2 a strength 2 CA with k columns in v levels, coded as integers 0, ..., v-1 or 1,...,v, same coding as \code{D3}; for v=2, \code{NULL} implies automatic creation with function \code{\link{KSK}}.
+#' @param D3 a strength 3 CA with k columns in v levels, coded as integers
+#'           0, ..., v-1 or 1,...,v
+#' @param D2 a strength 2 CA with k columns in v levels, coded as integers
+#'           0, ..., v-1 or 1,...,v, same coding as \code{D3};\cr
+#'           for v=2 or v=3, \code{NULL} implies automatic creation with function
+#'           \code{\link{KSK}} (v=2) or function \code{\link{CAEX}} (v=3).
 #' @param check a logical: if TRUE, the strength requirements are verified
 #' (may take very long for large arrays, therefore defaults to FALSE)
-#' @param start0 logical: Do integer values start with 0 ?  (\code{D3} and \code{D2} must be compatible)
+#' @param start0 logical: Do integer values start with 0 ?  (\code{D3} and \code{D2}
+#'           must be compatible)
 #' @param ... further arguments to function \code{\link{coverage}}
 #'
 #' @details
@@ -24,7 +29,7 @@
 #' or prime power and thus fulfills the conditions for the Bose
 #' construction of a strength 2 OA and the Bush
 #' construction of a strength 3 OA (as implemented in R package lhs),
-#' the designs are at present (13 Feb 2025) best-known for v>=5
+#' the designs are at present (13 Feb 2025) best-known for v >= 5
 #'
 #' @references Chateauneuf and Kreher (2002)
 #'
@@ -52,6 +57,31 @@
 #' dim(E)           ## size almost optimal
 #' eCAN(3, 12, 4)
 #'
+#' ## 3 levels, with default CAEX-design
+#' ## D3 is the current best-known CA(42, 3, 8, 3) from a cross-summing construction
+#' ##     by Colbourn et al. (2010, CKRS)
+#' eCAN(3, 8, 3)
+#'
+#' CA42.3.8.3 <- rbind(
+#' c(0,0,0,0,0,0,0,0), c(1,1,1,1,1,1,1,1), c(2,2,2,2,2,2,2,2), c(1,2,2,0,1,1,0,1),
+#' c(1,0,0,2,1,2,2,2), c(1,0,1,1,0,2,0,1), c(2,2,2,2,0,0,0,0), c(0,2,1,1,1,0,0,2),
+#' c(0,2,1,0,2,2,1,1), c(0,2,0,2,0,2,1,2), c(0,1,0,1,2,0,1,0), c(2,0,1,2,0,0,1,1),
+#' c(1,1,0,2,2,0,0,1), c(2,2,1,2,1,2,1,0), c(1,1,2,0,0,2,1,0), c(2,0,1,1,0,1,2,0),
+#' c(2,0,0,1,2,2,1,2), c(2,1,1,0,2,0,0,0), c(2,1,2,2,1,0,1,2), c(0,0,0,0,1,1,1,1),
+#' c(1,0,2,2,2,1,1,0), c(1,0,2,1,0,0,2,2), c(1,0,1,0,1,0,2,0), c(1,2,1,2,0,1,2,1),
+#' c(0,1,2,0,1,1,2,2), c(2,2,1,0,0,1,1,2), c(0,0,2,0,2,0,2,1), c(2,2,0,1,1,0,2,1),
+#' c(0,1,2,2,1,2,0,1), c(0,1,1,2,0,0,2,0), c(0,2,2,1,0,1,1,1), c(0,2,0,0,2,1,2,0),
+#' c(1,1,1,1,2,2,2,2), c(2,1,0,0,0,2,2,1), c(2,1,0,2,1,1,0,0), c(2,1,2,1,2,1,0,1),
+#' c(2,0,2,0,1,2,0,2), c(1,2,0,1,2,2,0,0), c(0,0,2,1,1,2,2,0), c(1,1,0,1,0,1,0,2),
+#' c(0,0,1,2,2,1,0,2), c(1,2,0,0,2,0,1,2)
+#' )
+#'
+#' ## D2=NULL implies use of CAEX(k=8), which has 13 runs
+#' E <- CK_doubling(CA42.3.8.3)
+#' dim(E)    ## 42 + 2*13 = 68 runs with 16 columns
+#' # coverage(E, 3)  ## commented out because of example run time
+#' eCAN(3, 16, 3)  ## run size too large by factor 4/3 versus the current optimum 51
+#'
 #' @export
 CK_doubling <- function(D3, D2=NULL, check=FALSE, start0=TRUE, ...){
   if (!is.matrix(D3)) D3 <- as.matrix(D3)
@@ -70,9 +100,12 @@ CK_doubling <- function(D3, D2=NULL, check=FALSE, start0=TRUE, ...){
     if (check) stopifnot(all(coverage(D2,2)==1))
     if (start0) stopifnot(min(D2)==0) else stopifnot(min(D2)==1)
   }
-  if (is.null(D2)) if (!v==2) stop("D2=NULL requires v=2")
-  if (v==2 && is.null(D2))
-    D2 <- KSK(k=k) + as.numeric(!start0)
+  if (is.null(D2)) if (!v %in% c(2,3)) stop("D2=NULL requires v=2 or v=3")
+  if (is.null(D2))
+    if (v==2)
+      D2 <- KSK(k=k) + as.numeric(!start0)
+    else  ## v==3
+      D2 <- CAEX(k=k) + as.numeric(!start0)
   if (!start0){
     D3 <- D3 - 1
     D2 <- D2 - 1
