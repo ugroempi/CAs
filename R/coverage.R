096093016a560c0852da94b0176f2804db42d9b7
ugroempi <groemping@beuth-hochschule.de>
2025-08-01 00:16:34 +0200

    new attempt at making parallel work on GWDG

--- R/coverage.R
+++ R/coverage.R
@@ -97,6 +97,7 @@
 #' @export
 coverage <- function(D, t, isInteger=TRUE,
                         verbose=0, start0=TRUE, parallel=1){
+  D <- D   ## make it part of this environment
   ## made faster by custom table function fasttab
   ## isInteger: are the values all from 0 to v-1 or from 1 to v?
   ## start0: 0 to v-1? (relevant for isI==TRUE only)
@@ -136,6 +137,7 @@ coverage <- function(D, t, isInteger=TRUE,
     stopifnot(parallel <= parallel::detectCores())
     mycl <- parallel::makePSOCKcluster(parallel)
     parallel::clusterExport(mycl, c("nproj", "projs", "D"), envir=sys.frame(which=1L))
+    parallel::clusterExport(mycl, c("fasttab"), envir=environment(CAs:::fasttab))
     tabs <- parallel::parLapply(mycl, 1:nproj,
                        function(obj) fasttab(D[,projs[,obj], drop=FALSE]))
     parallel::stopCluster(mycl); rm(mycl)
