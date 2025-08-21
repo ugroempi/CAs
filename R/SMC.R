e27d280ae7d00d7d71d7598c107e2c2448dfbc2d
ugroempi <groemping@beuth-hochschule.de>
2025-08-21 11:49:53 +0200

    implemented the Sherwood, Martirosyan and Colbourn (2006) construction with CPHFs and permutation vectors

---
+++ R/SMC.R
@@ -0,0 +1,158 @@
+## code for creating a CA from the CPHF
+#' Making CA from SCPHF
+#'
+#' according to the process outlined in Sherwood, Martirosyan and Colbourn (2006).
+#' It is assumed that the SCPHF refers to all non-reduced permutation vectors
+#' or to all reduced permutation vectors, but not to a mix of both.
+#'
+#' @rdname SMC
+#'
+#' @aliases smcCA
+#' @aliases SMC
+#' @aliases N_smcCA
+#' @aliases k_smcCA
+#'
+#' @usage smcCA(t, k, v, start0 = TRUE, ...)
+#' @usage SMC(cphf, q, t, ...)
+#' @usage N_smcCA(t, k, v)
+#' @usage k_smcCA(t, N, v)
+#'
+#' @param t integer: target strength
+#' @param k integer: requested number of columns
+#' @param v integer: number of levels, must be a prime power
+#' @param start0 logical: should the levels of the output array start with 0 (otherwise with 1)?
+#' @param cphf an SCPHF
+#' @param q integer: prime power
+#' @param N integer: affordable run size
+#' @param ... currently not used
+#'
+#' @returns Function \code{smcCA} returns a matrix of class \code{ca} with attributes,
+#' which is a covering array of strength 3 or 4.\cr
+#' Function \code{SMC} returns a matrix with entries in 0,...,v-1, which is a covering
+#' array of strength \code{t}, if a valid \code{cphf} for strength \code{t} is provided;
+#' the function is not meant for direct use, but is the workhorse behind \code{smcCA}.
+#'
+#' Functions \code{N_smcCA} and \code{k_smcCA} return the required run size or the achievable number of columns, respectively.
+#'
+#' @section Details:
+#' Function \code{SMC} uses a covering perfect hash family of a specific form (SCPHF)
+#' that works with length \code{v^t} permutation vectors for \code{v} levels
+#' and strength \code{t}. Function \code{smcCA} implements CAs from this
+#' construction for the SCPHFs from Sherwood et al. (2006).
+#' The construction relies on a Galois field GF(\code{v}).
+#' The SCPHFs for \code{v=8} or \code{v=9} require a different Galois field
+#' than otherwise used in this package (created with internal function \code{mygf}).
+#'
+#' The data for the construction are in \code{\link{SMC_CPHFs}} and \code{\link{SMCcat}},
+#' respectively.
+#'
+#' @references Sherwood, Martirosyan and Colbourn (2006)
+#'
+#' @examples
+#' # applying the construction for small SCPHF from the Sherwood et al. (2006) paper
+#'
+#' # strength 3
+#' cphf <- rbind(0:15,
+#'               c(0,1,4,5,2,3,6,7,9,8,13,12,11,10,15,14))
+#' D <- SMC(cphf, q=4, t=3)
+#' dim(D)
+#' coverage(D,3)
+#' eCAN(3,16,4)   ## size matches eCAN
+#'
+#' # strength 4
+#' cphf <- rbind(0:7,
+#'               c(0, 1, 2, 4, 3, 6, 7, 5))
+#' D <- SMC(cphf, q=2, t=4)
+#' dim(D)
+#' coverage(D,4)
+#' eCAN(4,8,2)   ## size larger than eCAN
+#'
+#' @importFrom sfsmisc digitsBase
+
+#' @export
+smcCA <- function(t, k, v, start0=TRUE, ...){
+  Call <- sys.call()
+  stopifnot(is.numeric(t), is.numeric(k), is.numeric(v))
+  zeilen <- SMCcat[SMCcat$t>=t & SMCcat$k>=k & SMCcat$v==v,]
+  ## lower strength is at the top,
+  ## higher strength at the bottom
+  zrows <- nrow(zeilen)
+  if (zrows == 0) stop("The request cannot be served by function smcCA.")
+  zeile <- zeilen[zrows + 1 - which.min(rev(zeilen$N)), ,drop=FALSE]
+  vc <- as.character(zeile$v); tc <- as.character(zeile$t); kc <- as.character(zeile$k)
+  aus <- SMC(SMC_CPHFs[[tc]][[vc]][[kc]], zeile$v, zeile$t)
+  if (!start0) aus <- aus + 1
+  aus[,1:k]
+  class(aus) <- c("ca", class(aus))
+  attr(aus, "Call") <- Call
+  attr(aus, "origin") <- paste0("Sherwood, Martirosyan, Colbourn (2006), strength t=", t, ", ")
+  attr(aus, "t") <- t
+  aus
+}
+
+#' @export
+SMC <- function(cphf, q, t, ...){
+  # cphf a strength t SCPHF
+  # in q^t levels
+  ## matrix with the beta vectors in the order 0 top
+  ## to t-1 bottom
+
+  if (q==8) gf <- mygf(q, 11) else gf <- lhs::create_galois_field(q)
+  ## the cphf for q=8 does not work with the field from lhs
+  ##  but needs a different isomorphic variant of the gf
+
+  ## coefficients for low powers at the top
+
+  ## lspace provides the h tuples for the CA columns
+  lspace <- sfsmisc::digitsBase(0:(q^(t-1)-1), q, ndigits=t-1)[(t-1):1,]
+  ## betaspace contains the beta coefficients
+
+  betaspace <- sfsmisc::digitsBase(0:(q^t-1), q, ndigits=t)[t:1,]
+
+  CA <- gf_matmult(t(betaspace), rbind(1, lspace), gf)
+  DHF2CA(cphf, CA)
+}
+
+#' @export
+N_smcCA <- function(t, k, v){
+  stopifnot(is.numeric(t), is.numeric(k), is.numeric(v))
+  zeilen <- SMCcat[SMCcat$t>=t & SMCcat$k>=k & SMCcat$v==v,]
+  ## lower strength is at the top,
+  ## higher strength at the bottom
+  zrows <- nrow(zeilen)
+  if (zrows == 0) return(NA)
+  zeile <- zeilen[zrows + 1 - which.min(rev(zeilen$N)), ,drop=FALSE]
+  min(zeilen$N)
+}
+
+#' @export
+k_smcCA  <- function(t, N, v){
+  hilf <- SMCcat[SMCcat$t==t & SMCcat$N<=N &
+                               SMCcat$v==v,,drop=FALSE]
+  if (nrow(hilf)==0) return(NA)
+  max(hilf[,"k"])
+}
+
+SMC9 <- function(cphf, q, t, primitive, ...){
+  ## experiment with the unsolved cphf
+  ## no longer needed, keep for similar cases
+
+  # cphf a strength t SCPHF
+  # in q^t levels
+  ## matrix with the beta vectors in the order 0 top
+  ## to t-1 bottom
+
+  if (!q==9) stop("for q=9 only")
+  gf <- mygf(q, primitive)
+  print(switch(as.character(primitive), '14'="default GF",
+               '10'="x^2 + 1", '17'="x^2 + 2x + 2", "???"))
+
+  ## lspace provides the h tuples for the CA columns
+  lspace <- sfsmisc::digitsBase(0:(q^(t-1)-1), q, ndigits=t-1)[(t-1):1,]
+  ## betaspace contains the beta coefficients
+
+  betaspace <- sfsmisc::digitsBase(0:(q^t-1), q, ndigits=t)[t:1,]
+
+  CA <- gf_matmult(t(betaspace), rbind(1, lspace), gf)
+  DHF2CA(cphf, CA)
+}
---
+++ inst/extdata/SherwoodMartirosyanColbournCPHF.R
@@ -0,0 +1,192 @@
+
+transform <- function(cphf_string, s){
+  ## assumes that the symbol q-1 occurs
+  ## pow is the power so that the number of levels is v=q^s
+  ## as 0, ..., v-1
+  hilf <- strsplit(cphf_string, " ")
+  hilf <- lapply(hilf, function(obj)
+    sapply(obj, function(obj2)
+      unlist(strsplit(obj2, ""))))
+  alphabet <- sort(unique(unlist(hilf)))
+  if ("A" %in% alphabet) hilf <- lapply(hilf,
+                                        function(obj) {obj[which(obj=="A")] <- "10"; obj})
+  if ("B" %in% alphabet) hilf <- lapply(hilf,
+                                        function(obj) {obj[which(obj=="B")] <- "11"; obj})
+  if ("C" %in% alphabet) hilf <- lapply(hilf,
+                                        function(obj) {obj[which(obj=="C")] <- "12"; obj})
+  if ("D" %in% alphabet) hilf <- lapply(hilf,
+                                        function(obj) {obj[which(obj=="D")] <- "13"; obj})
+  if ("E" %in% alphabet) hilf <- lapply(hilf,
+                                        function(obj) {obj[which(obj=="E")] <- "14"; obj})
+  if ("F" %in% alphabet) hilf <- lapply(hilf,
+                                        function(obj) {obj[which(obj=="F")] <- "15"; obj})
+  alphabet <- sort(as.numeric(unique(unlist(hilf))))
+  q <- max(alphabet) + 1
+  t(sapply(hilf, function(obj){
+    dd <- dim(obj)
+    obj <- matrix(as.numeric(obj), nrow=dd[1])
+    t(q^((s - 1) : 0)) %*% obj
+  }))
+}
+
+## copy-pasted from Sherwood, Martirosyan, Colbourn (2006)
+  ############## strength t=3 ####################################
+#CPHF(3; 20, 9, 3)  # v=3 # paper: N=75, k=20
+SMC_CPHFs <- list(
+   '3'=vector(mode="list", 0),
+   '4'=vector(mode="list", 0)
+)
+cphf <- c("00 01 02 10 11 12 20 21 22 00 01 02 10 11 12 20 21 22 00 01",
+          "00 01 02 10 11 22 20 21 12 22 20 21 12 10 01 02 00 11 20 00",
+          "00 01 10 02 11 21 22 12 20 10 11 01 00 12 20 02 22 21 20 21")
+(cphf <- transform(cphf, 2))
+## coverage OK
+SMC_CPHFs$`3`[["3"]] <- list("20"=cphf)
+
+#CPHF(2; 16, 16, 3)  # v=4 # paper: N=124, k=16
+cphf <- c("00 01 02 03 10 11 12 13 20 21 22 23 30 31 32 33",
+          "00 01 10 11 02 03 12 13 21 20 31 30 23 22 33 32")
+(cphf1 <- transform(cphf, 2))
+## coverage OK
+
+#CPHF(3; 28, 16, 3)  # v=4 # paper: N=184, k=28
+cphf <- c("00 01 02 03 10 11 12 13 20 21 22 23 30 31 32 33 00 01 02 03 10 11 12 13 20 21 22 23",
+          "00 01 02 03 10 11 12 13 20 21 22 23 30 31 33 32 11 10 22 23 01 00 20 21 32 33 02 03",
+          "00 01 10 11 02 03 12 13 21 20 31 30 23 22 32 33 11 10 01 00 31 30 13 12 22 23 02 03")
+(cphf2 <- transform(cphf, 2))
+## coverage OK
+SMC_CPHFs$`3`[["4"]] <- list("16"=cphf1, "28"=cphf2)
+
+#CPHF(2; 24, 25, 3)  # v=5 # paper: N=245, k=24
+cphf <- c("00 01 02 03 04 10 11 12 13 14 20 21 22 23 24 30 31 32 33 34 40 41 42 43",
+          "00 01 10 11 23 04 42 03 13 24 14 02 31 41 43 22 30 21 40 32 12 44 33 34")
+(cphf <- transform(cphf, 2))
+## coverage OK
+SMC_CPHFs$`3`[["5"]] <- list("24"=cphf)
+
+#CPHF(2; 31, 49, 3)  # v=7 # paper: N=679, k=31
+cphf <- c("00 01 02 03 04 05 06 10 11 12 13 14 15 16 20 21 22 23 24 25 26 30 31 32 33 34 35 36 40 41 42",
+          "00 01 10 11 23 25 63 02 03 12 13 20 60 65 22 43 46 62 55 16 53 50 52 61 42 15 31 40 34 35 41")
+(cphf <- transform(cphf, 2))
+## coverage OK
+SMC_CPHFs$`3`[["7"]] <- list("31"=cphf)
+
+#CPHF(2; 40, 64, 3)  # v=8 # paper: N=1016, k=40
+cphf <- c("00 01 02 03 04 05 06 07 10 11 12 13 14 15 16 17 20 21 22 23 24 25 26 27 30 31 32 33 34 35 36 37 40 41 42 43 44 45 46 47",
+          "00 01 10 11 24 25 34 35 02 03 12 13 26 27 36 37 21 20 31 30 05 04 15 14 23 22 33 32 07 06 17 16 51 50 41 40 75 74 65 64")
+(cphf <- transform(cphf, 2))
+## this uses a different primitive polynomial than package lhs for the Galois field:
+##     lhs uses x^3 = 1 + x^2 (RHS in xton)
+##         which is equivalent to x^3 - x^2 - 1 = 0
+##         which is equivalent to x^3 + x^2 + 1 = 0 in terms of GF(2),
+##         i.e., primitive element 13
+##     presumably, the paper uses x^3 + x + 1 = 0, i.e., primitive element 11
+##
+
+## with the correct galois field, it works with all 40 columns
+## with the default one, only with the first 32 columns
+SMC_CPHFs$`3`[["8"]] <- list("40"=cphf)
+
+#CPHF(2; 39, 81, 3)  # v=9 # paper: N=1449, k=39
+cphf <- c("00 01 02 03 04 05 06 07 08 10 11 12 13 14 15 16 17 18 20 21 22 23 24 25 26 27 28 30 31 32 33 34 35 36 37 38 40 41 42",
+          "00 01 10 11 24 26 53 57 83 02 03 12 13 20 25 46 48 66 14 15 71 64 30 80 76 85 68 74 67 08 51 41 47 78 62 05 21 52 84")
+(cphf <- transform(cphf, 2))
+## this also does not work with the primitive polynomial of package lhs for the Galois field:
+##     lhs uses x^2 = 1 + 2x (RHS in xton)
+##         which is equivalent to x^2 - 2x - 1 = 0
+##         which is equivalent to x^2 + x + 2 = 0 in terms of GF(3),
+##         i.e., primitive element 14
+## the suitable characteristic polynomial is x^2+2x+2
+##
+SMC_CPHFs$`3`[["9"]] <- list("39"=cphf)
+
+
+#CPHF(2; 44, 121, 3) # v=11 # paper: N=2651, k=44
+cphf <- c("00 01 02 03 04 05 06 07 08 09 0A 10 11 12 13 14 15 16 17 18 19 1A 20 21 22 23 24 25 26 27 28 29 2A 30 31 32 33 34 35 36 37 38 39 3A",
+          "00 01 10 11 24 28 43 49 83 89 A4 02 03 12 13 26 2A 40 45 80 85 A6 14 21 20 04 0A A7 70 77 19 51 58 50 64 35 86 96 A0 A5 34 99 68 88")
+(cphf <- transform(cphf, 2))
+## coverage OK
+SMC_CPHFs$`3`[["11"]] <- list("44"=cphf)
+
+
+#CPHF(2; 39, 169, 3) # v=13
+cphf <- c(
+  "00 01 02 03 04 05 06 07 08 09 0A 0B 0C 10 11 12 13 14 15 16 17 18 19 1A 1B 1C 20 21 22 23 24 25 26 27 28 29 2A 2B 2C",
+  "00 01 10 11 24 2A 45 49 57 97 A5 A9 C4 02 03 12 13 26 2C 47 4B 59 99 A7 AB C6 14 23 20 06 0A 1C 73 84 70 8C B2 B1 9A")
+(cphf <- transform(cphf, 2))
+# coverage(SMC(cphf,13, 3),3, parallel = 4)
+# covers
+
+SMC_CPHFs$`3`[["13"]] <- list("39"=cphf)
+
+
+############## strength t=4 ################
+#CPHF(2; 8, 8, 4)  # v=2 # paper: N=30, k=8
+cphf <- c(
+  "000 001 010 011 100 101 110 111",
+  "000 001 010 100 011 110 111 101")
+(cphf1 <- transform(cphf, 3))
+
+
+#CPHF(4; 11, 8, 4) # v=2 # paper: N=58, k=11
+cphf <- c("000 001 010 011 100 101 110 111 000 001 010",
+          "000 001 010 011 100 101 111 110 101 111 001",
+          "000 001 010 011 100 110 111 101 111 010 101",
+          "000 001 010 100 011 111 101 110 011 100 000")
+(cphf2 <- transform(cphf, 3))
+
+SMC_CPHFs$`4`[["2"]] <- list("8"=cphf1,
+                              "11"=cphf2)
+
+#CPHF(2; 10, 27, 4)   # v=3 # paper: N=159, k=10
+cphf <- c("000 001 002 010 011 100 101 110 120 121",
+          "000 001 010 100 111 101 211 122 212 112")
+(cphf1 <- transform(cphf, 3))
+
+#CPHF(3; 16, 27, 4)   # v=3 # paper: N=237, k=16
+cphf <- c("000 001 002 010 011 012 020 021 100 101 102 110 111 112 211 222",
+          "000 001 002 010 011 100 101 110 012 020 221 112 220 022 111 122",
+          "000 001 010 100 111 101 211 120 200 121 221 201 220 210 110 102")
+(cphf2 <- transform(cphf, 3))
+SMC_CPHFs$`4`[["3"]] <- list("10"=cphf1,
+                              "16"=cphf2)
+
+
+#CPHF(2; 9, 64, 4)    # v=4 # paper: N=508, k=9
+cphf <- c("000 001 002 003 010 100 110 121 131",
+          "000 001 010 100 112 113 121 211 222")
+(cphf <- transform(cphf, 3))
+SMC_CPHFs$`4`[["4"]] <- list("9"=cphf)
+
+
+#CPHF(2; 11, 125, 4)  # v=5 # paper: N=1245, k=11
+cphf <- c("000 001 002 003 004 010 100 110 121 212 231",
+          "000 001 010 100 111 124 142 214 241 412 421")
+(cphf <- transform(cphf, 3))
+SMC_CPHFs$`4`[["5"]] <- list("11"=cphf)
+
+SMCcat <- data.frame(t=numeric(0), v=numeric(0), k=numeric(0), N=numeric(0))
+for (t in 3:4)
+  for (v in as.numeric(names(SMC_CPHFs[[as.character(t)]]))){
+    for (k in as.numeric(names(SMC_CPHFs[[as.character(t)]][[as.character(v)]]))){
+      kc <- as.character(k); vc <- as.character(v); tc <- as.character(t)
+      SMCcat <- rbind(SMCcat,
+      c(t=t, v=v, k=k,
+        N=nrow(SMC(SMC_CPHFs[[tc]][[vc]][[kc]], v, t))))
+    }
+  }
+colnames(SMCcat) <- c("t","v", "k", "N")
+
+save("SMCcat", file="d:/rtests/CAs/data/SMCcat.rda", compress="xz")
+save("SMC_CPHFs", file="d:/rtests/CAs/data/SMC_CPHFs.rda", compress="xz")
+
+## check that everything is correct
+for (t in 3:4)
+  for (v in as.numeric(names(SMC_CPHFs[[as.character(t)]]))){
+    for (k in as.numeric(names(SMC_CPHFs[[as.character(t)]][[as.character(v)]]))){
+      print(paste0("t=",t, ", v=", v, ", k=", k))
+      kc <- as.character(k); vc <- as.character(v); tc <- as.character(t)
+      cphf <- SMC_CPHFs[[tc]][[vc]][[kc]]
+      print(coverage(SMC(cphf, v, t), t, parallel=4))
+          }
+  }
